71c43a58e7ad5f05e05fec6ebd7c1a3dd5bbbaab
Viper <3088663751@qq.com>
2025-12-29 14:46:33 +0800

    实现新增套餐

---
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -0,0 +1,14 @@
+package com.sky.service;
+
+import com.sky.dto.SetmealDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface SetmealService {
+
+    /**
+     * 新增套餐及对应的菜品
+     * @param setmealDTO
+     */
+    void saveWithDishes(SetmealDTO setmealDTO);
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -0,0 +1,49 @@
+package com.sky.service.impl;
+
+import com.sky.dto.SetmealDTO;
+import com.sky.entity.Setmeal;
+import com.sky.entity.SetmealDish;
+import com.sky.mapper.SetmealDishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.service.SetmealService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class SetmealServiceImpl implements SetmealService {
+
+    @Autowired
+    private SetmealMapper setmealMapper;
+    @Autowired
+    private SetmealDishMapper setmealDishMapper;
+
+    /**
+     * 新增套餐及对应的菜品
+     * @param setmealDTO
+     */
+    @Transactional
+    public void saveWithDishes(SetmealDTO setmealDTO) {
+        //插入套餐基本信息
+        Setmeal setmeal = new Setmeal();
+        BeanUtils.copyProperties(setmealDTO,setmeal);
+
+        setmealMapper.insert(setmeal);
+
+
+        //获取insert回填的套餐id
+        Long setmealId = setmeal.getId();
+
+        List<SetmealDish> setmealDishes = setmealDTO.getSetmealDishes();
+        if(setmealDishes != null && setmealDishes.size() > 0){
+            setmealDishes.forEach(setmealDish -> {
+                setmealDish.setSetmealId(setmealId);
+            });
+            //批量插入套餐内菜品信息
+            setmealDishMapper.insertBatch(setmealDishes);
+        }
+    }
+}
