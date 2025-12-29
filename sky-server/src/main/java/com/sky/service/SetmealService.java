5e36d349a1daef24bf5409d6ac52d6a4d017968d
Viper <3088663751@qq.com>
2025-12-29 15:08:29 +0800

    实现套餐分页查询

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,4 +13,10 @@ public interface SetmealService {
      * @param setmealDTO
      */
     void saveWithDishes(SetmealDTO setmealDTO);
+
+    /**
+     * 套餐分页查询
+     * @return
+     */
+    PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
 }
