0fc8b693fdac796fb0c2ffcbc2d1ad0c74f7d508
Viper <3088663751@qq.com>
2026-01-02 01:02:44 +0800

    实现清空购物车

--- sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -45,4 +45,16 @@ public class ShoppingCartController {
         List<ShoppingCart> shoppingCarts = shoppingCartService.list();
         return Result.success(shoppingCarts);
     }
+
+    /**
+     * 清空购物车
+     * @return
+     */
+    @DeleteMapping("/clean")
+    @ApiOperation("清空购物车")
+    public Result clean(){
+        log.info("清空购物车");
+        shoppingCartService.cleanShoppingCart();
+        return Result.success();
+    }
 }
